1ee6a0e75afc9e44796cea8a39d6034e1c06ce5e
EminaMuratovic <emina_peace@hotmail.com>
2015-02-22 15:02:43 +0100

    komentari

--- src/com/ebay/model/Category.java
+++ src/com/ebay/model/Category.java
@@ -1,48 +1,71 @@
 package com.ebay.model;
 
+/**
+ * This class creates a category
+ * If the category has no parent(it's a main category), parent_id sets to null
+ * @author BITBay
+ *
+ */
 public class Category {
 	
 	public String name;
 	public int parent_id;
 	public int category_id;
 	
+	/**
+	 * creates a category
+	 * @param name String name of the category
+	 * @param parent_id int id of the parent category
+	 * @param category_id id of this category
+	 */
 	public Category(String name, int parent_id, int category_id){};
 	
+	/**
+	 * saves the category to database
+	 * @return true or false
+	 */
 	public boolean save(){
 		return false;
 		}
 	
+	/**
+	 * updates a category to database
+	 */
 	public void update() {};
 	
-	public static void delete() {};
+	/**
+	 * deletes a category from the database
+	 */
+	public static void delete() {}
 
+	/**
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * @param name the name to set
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * @return the parent_id
+	 */
 	public int getParent_id() {
 		return parent_id;
 	}
 
-	public void setParent_id(int parent_id) {
-		this.parent_id = parent_id;
-	}
-
+	/**
+	 * @return the category_id
+	 */
 	public int getCategory_id() {
 		return category_id;
-	}
-
-	public void setCategory_id(int category_id) {
-		this.category_id = category_id;
 	};
+
 	
-	
-	
+
 }
-
-
-	
