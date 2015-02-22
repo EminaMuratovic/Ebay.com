a286ddea60edaa157e462a272b205951c1d115fa
EminaMuratovic <emina_peace@hotmail.com>
2015-02-22 15:04:18 +0100

    komentari

--- src/com/ebay/model/Order_details.java
+++ src/com/ebay/model/Order_details.java
@@ -1,36 +1,54 @@
 package com.ebay.model;
 
+/**
+ * This class is extended from the class Order
+ * It gives us main order details
+ * @author BITBay
+ *
+ */
 public class Order_details {
 	public int order_id;
 	public int product_id;
 	
+	/**
+	 * Creates order details
+	 * @param order_id int id of the order
+	 * @param product_id int id of the product
+	 */
 	public Order_details(int order_id, int product_id) {}
 	
+	/**
+	 * saves the order details to database
+	 * @return true or false
+	 */
 	public boolean save(){
 		return false;
 		}
 	
+	/**
+	 * updates the order details to database
+	 */
 	public void update() {};
 	
-	public static void delete() {};
+	/**
+	 * deletes the order details from database
+	 */
+	public static void delete() {}
 
-
+	/**
+	 * @return the order_id
+	 */
 	public int getOrder_id() {
 		return order_id;
 	}
 
-	public void setOrder_id(int order_id) {
-		this.order_id = order_id;
-	}
-
+	/**
+	 * @return the product_id
+	 */
 	public int getProduct_id() {
 		return product_id;
-	}
-
-	public void setProduct_id(int product_id) {
-		this.product_id = product_id;
 	};
-	
+
 	
 	
 }
--- src/com/ebay/model/Shippment.java
+++ src/com/ebay/model/Shippment.java
@@ -1,54 +1,85 @@
 package com.ebay.model;
 
+/**
+ * This class separates the countries and their shippment id and shippment price
+ * @author BITBay
+ *
+ */
 public class Shippment {
 	
 	public String type;
-	public int price;
+	public float price;
 	public int country_id;
 	public int shippment_id;
 	
+	/**
+	 * Creates a shippment
+	 * @param type String type of the shippment
+	 * @param price float price of the shippment
+	 * @param country_id int id of the country
+	 * @param shippment_id int id of the shippment
+	 */
 	public Shippment(String type, int price, int country_id, int shippment_id) {}
 	
+	/**
+	 * saves the shippment to database
+	 * @return true or false
+	 */
 	public boolean save(){
 		return false;
 		}
 	
+	/**
+	 * updates the shippment to database
+	 */
 	public void update() {};
 	
-	public static void delete() {};
+	/**
+	 * deletes the shippment from database
+	 */
+	public static void delete() {}
 
-
+	/**
+	 * @return the type
+	 */
 	public String getType() {
 		return type;
 	}
 
+	/**
+	 * @param type the type to set
+	 */
 	public void setType(String type) {
 		this.type = type;
 	}
 
-	public int getPrice() {
+	/**
+	 * @return the price
+	 */
+	public float getPrice() {
 		return price;
 	}
 
-	public void setPrice(int price) {
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(float price) {
 		this.price = price;
 	}
 
+	/**
+	 * @return the country_id
+	 */
 	public int getCountry_id() {
 		return country_id;
 	}
 
-	public void setCountry_id(int country_id) {
-		this.country_id = country_id;
-	}
-
+	/**
+	 * @return the shippment_id
+	 */
 	public int getShippment_id() {
 		return shippment_id;
-	}
-
-	public void setShippment_id(int shippment_id) {
-		this.shippment_id = shippment_id;
 	};
-	
+
 	
 }
