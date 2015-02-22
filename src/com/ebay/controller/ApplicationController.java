2dadfcd44ea0e0c602bd84ec0c56e72ebd898648
mustafa <mustafa.ademovic93@gmail.com>
2015-02-22 13:56:28 +0100

    Added ApplicationController class

---
+++ src/com/ebay/controller/ApplicationController.java
@@ -0,0 +1,15 @@
+package com.ebay.controller;
+
+public class ApplicationController {
+	
+	public static void main(String[] args) {};
+	
+	public static void edit() {};
+	
+	public static void update() {};
+	
+	public static void delete() {};
+
+	public static void create(){}
+
+}
